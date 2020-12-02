20a586fddea27a04cfdae5228c09f2f6f6ce8642
Cooper Viktor <im.cooperviktor@gmail.com>
2020-12-02 17:04:35 +1100

    product page now populating data from backend. json data array deleted

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
-	const product = products.find((p) => p._id === match.params.id);
+	const [product, setProduct] = useState({});
+	const id = match.params.id;
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get(`/api/products/${id}`);
+			console.log(data);
+
+			setProduct(data);
+		};
+
+		fetchProducts();
+	}, []);
 
 	return (
 		<div>
