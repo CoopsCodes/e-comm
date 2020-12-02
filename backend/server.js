0ab42be801096f3168110127bb954c412ca4cfc0
Cooper Viktor <im.cooperviktor@gmail.com>
2020-12-02 16:51:16 +1100

    loading frontend data from backend server

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,19 @@ const express = require("express");
 const PORT = 5000;
 const app = express();
 
+const products = require("./data/products");
+
+app.get("/", (req, res) => {
+	res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+	res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
+});
+
 app.listen(PORT, console.log(`Server Running on PORT ${PORT}`));
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
-import products from "../products";
+import { useState, useEffect } from "react";
 import Product from "../components/Product";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get("/api/products");
+
+			setProducts(data);
+		};
+
+		fetchProducts();
+	}, []);
+
 	return (
 		<div>
 			<h1>Latest Products</h1>
