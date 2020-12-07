26d6740b4e8e90f48536ba949a8a5c6162fc9045
Cooper Viktor <im.cooperviktor@gmail.com>
2020-12-07 16:17:03 +1100

    backend refactored with middleware and 404 fallbacks

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +18,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(
 	PORT,
