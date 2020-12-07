6716a8f2f9341be01176deccbb83318f0299bacc
Cooper Viktor <im.cooperviktor@gmail.com>
2020-12-07 12:40:49 +1100

    refactoring api routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+import Product from "../models/productModel.js";
+
+// Fetch All Products
+// GET /api/products
+// Public
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	})
+);
+
+// Fetch One Product
+// GET /api/products/:id
+// Public
+router.get(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404).json({ message: "Product not found" });
+		}
+	})
+);
+
+export default router;
